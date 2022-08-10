cb9cdb39309c7874f8b98c0e2951287b36a55553
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-10 16:21:29 -0700

    added places view with mock data

---
+++ views/places/index.jsx
@@ -0,0 +1,12 @@
+const React = require('react')
+const Def = require('./index')
+
+function index () {
+    return(
+        <Def>
+            <main>
+                <h1>places index page</h1>
+            </main>
+        </Def>
+    )
+}
