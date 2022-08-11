a9a90ed81372ec51edd2c3e370719d614079cb17
Ronaldo Barajas <rbarajas_8@hotmail.com>
2022-08-10 17:02:22 -0700

    added PORT.env

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+const PORT= process.env.PORT
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 
@@ -10,6 +12,25 @@ app.use('/places', require('./controllers/places'))
 app.get('/', (req, res) => {
   res.render('home')
 })
+
+// GET /places
+app.get('/', (req, res) => {
+    let places = [{
+        name: 'H-Thai-ML',
+        city: 'Seattle',
+        state: 'WA',
+        cuisines: 'Thai, Pan-Asian',
+        pic: 'http://placekitten.com/250/250'
+    }, {
+        name: 'Coding Cat Cafe',
+        city: 'Phoenix',
+        state: 'AZ',
+        cuisines: 'Coffee, Bakery',
+        pic: 'http://placekitten.com/250/250'
+    }]      
+    res.render('places/index', {places})
+  })
+  
 
 app.get('*', (req, res) => {
   res.render('error')
